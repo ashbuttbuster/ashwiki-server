0fc97d7ccd9b34c43a520d8fcaf309b025bfaf69
ash_buttbuster <1n7n37@gmail.com>
2022-10-01 15:59:00 +0000

    Add files via upload

--- profile.py
+++ profile.py
@@ -9,12 +9,27 @@ from flask import request
 
 import sqlutils
 
+def getProfileID(login):
+    return sqlutils.selectQuery('profile',['profile_id','login'],f'login="{login}"')[0]['profile_id']
+
 def generateToken(login, expire=7):
     key = random.randbytes(25).hex()
     ip = request.environ['HTTP_X_FORWARDED_FOR']
     expire_date = datetime.now() + timedelta(expire)
-    profile_id = sqlutils.selectQuery('profile',['profile_id','login'],f'login="{login}"')[0]['profile_id']
+    profile_id = getProfileID(login)
     sqlutils.insertQuery('token',['id','profile','user_ip','expire_date'],[[key,str(profile_id),ip,expire_date.strftime("%Y-%m-%d")]])
+
+def validateToken(login,ip):
+    profile_id = getProfileID(login)
+    tokens = sqlutils.selectQuery('token',['id','profile','user_ip','expire_date'],f'profile={profile_id}')
+    if len(tokens) > 1:
+        return False, "Несколько токенов на один профиль."
+    elif tokens[0]['user_ip'] != ip:
+        return False, "IP пользователя отличается от IP, привязанный к токену"
+    elif datetime.now() > datetime.strptime(tokens[0]['expire_date'],"%Y-%m-%d"):
+        return False, "Срок действия токена истек"
+    else:
+        return True
 
 def identProfile():
     return request.cookies.get('login')
